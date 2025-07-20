4227f13e61d11ef7835bc6bac7fc9bcec6f51d0d
abstruse-scientia <priyanshu0496@gmail.com>
2025-07-20 15:46:43 +0530

    feat: add complete Spring boot backend -Add base project, structure under backendSection -Include controller, service, DTO, repository and entity layers -Configure application.properties and SQL schema

---
+++ backendSection/mystore/src/main/java/com/scientia/mystore/dto/ProductDto.java
@@ -0,0 +1,25 @@
+package com.scientia.mystore.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+
+@Getter
+@Setter
+public class ProductDto {
+
+    private Long productId;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private Integer popularity;
+
+    private String imageUrl;
+
+    private Instant createdAt;
+}
