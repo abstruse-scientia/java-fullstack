1b6cea887d1c2bebe84a244178a7e8431ce87ba7
abstruse-scientia <priyanshu0496@gmail.com>
2025-08-15 01:30:10 +0530

    feat: Add Proflie view

---
+++ section14/mystore-ui/src/api/apiClient.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
+});
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    const jwtToken = localStorage.getItem("jwtToken");
+    if (jwtToken) {
+      config.headers.Authorization = `Bearer ${jwtToken}`;
+    }
+    return config
+  },
+
+  (error)=> Promise.reject(error)
+)
+
+export default apiClient;
